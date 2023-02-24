a8d03b9a95c1097673fd107399b24a14a6f8b94e
pinalreliution <pinalsreliution@gmail.com>
2023-02-24 18:44:06 +0530

    Add file

--- exerciseE001_V3.py
+++ exerciseE001_V3.py
@@ -3,16 +3,16 @@
     Create one class named “location” with members “name”, “code”.  #done
     Create one class named “movement” with members “from_location”, “to_location”, “product”, “quantity”.  #done
     Create one static method named “movements_by_product” inside the “movement” class with one argument named “product”.  #done
-        This method will return all “movement” objects which belong to the passed “product” as an argument.
+        This method will return all “movement” objects which belong to the passed “product” as an argument.     #done
     Add new members inside the product “stock_at_locations”.        #done
-        This new member is a type of Dictionary, and it contains “location” as key and actual stock of that product on that location as value.
+        This new member is a type of Dictionary, and it contains “location” as key and actual stock of that product on that location as value.  #done
     Create 4 different location objects.        #done
     Create 5 different product objects.     #done
     Move those 5 products from one location to another location using movement.
         Manage exceptions if product stock goes in -ve.
-    Display movements of each product using the “movement_by_product” method.
+    Display movements of each product using the “movement_by_product” method.       #done
     Display product details with its stock at various locations using “stock_at_locations”.
-    Display product list by location ( group by location).
+    Display product list by location ( group by location).      #done
 '''
 
 
@@ -22,8 +22,21 @@ class Location:
         self.name = name
         self.code = code
 
+    @staticmethod
+    def sort(l_list):
+        for loc in range(len(l_list)):
+            for loc1 in range(loc + 1, len(l_list)):
+                if l_list[loc].name > l_list[loc1].name:
+                    l_list[loc], l_list[loc1] = l_list[loc1], l_list[loc]
+        for loc in l_list:
+            print("Location name:", loc.name)
+            for product1 in mov_list:
+                if loc == product1.from_location:
+                    print("Product details :", "Product name :", '|', product1.product.name, '|', "Code :", product1.product.code, '|', "Price :", product1.product.price, '|', "Stock of products :", product1.product.stock_at_locations)
+            print()
+
     def show_location(self):
-        print("Location name : ", self.name, "Code : ", self.code)
+        print("Location name : ", self.name, '\n', "Code : ", self.code)
 
 
 class Movement:
@@ -33,18 +46,17 @@ class Movement:
         self.to_location = to_location
         self.product = product
         self.quantity = quantity
+        product.stock_at_locations.update({str(self.from_location.name): str(self.quantity)})
 
     @staticmethod
     def movements_by_product(product):
-        x = input("Insert the product : ")
-        if x == product:
-            return Movement.show_movement()
-        else:
-            return print("Nothing")
+        for pro in mov_list:
+            if pro.product == product:
+                pro.show_movement()
 
     def show_movement(self):
         print("From location : ", self.from_location.name, '|', "To location :", self.to_location.name, '|',
-              "Product :", self.product.name, '|', "Quantity :", self.quantity)
+              "Product :", self.product.name, '|', "Quantity :", str(self.quantity))
 
 
 class Product:
@@ -53,53 +65,54 @@ class Product:
         self.name = name
         self.code = code
         self.price = price
-        self.stock_at_locations = dict()
+        self.stock_at_locations = {}
 
-    def add(self, key, value):
-        self.stock_at_locations[key] = value
+    def stock(self):
+        pass
 
-    def details(self):
-        return print("Product name :", self.name, '|', "Code :", self.code, '|',
-                     "Price :", self.price, "Stock at locations :", self.stock_at_locations)
+    def show_product(self):
+        return print("Product name :", self.name, '|', "Code :", str(self.code), '|',
+                     "Price :", str(self.price), '|', "Stock at locations :", self.stock_at_locations)
 
-
-# Create product class object
-p1 = Product("Airconditioner", 101, 450000)
-p2 = Product("Cloths", 201, 3600)
-p3 = Product("Chair", 301, 890000)
-p4 = Product("Book", 401, 455874)
-p5 = Product("Laptop", 501, 45789)
 
 # Create location class object
-l1 = Location("Rajkot", 1001)
-l2 = Location("Jamnagar", 2001)
-l3 = Location("Ahmadabad", 3001)
-l4 = Location("Baroda", 4001)
+rajkot = Location("Rajkot", 1001)
+jamnagar = Location("Jamnagar", 2001)
+ahmadabad = Location("Ahmadabad", 3001)
+baroda = Location("Baroda", 4001)
+
+# Create product class object
+airconditioner = Product("Airconditioner", 101, 450000)
+cloths = Product("Cloths", 201, 3600)
+chair = Product("Chair", 301, 890000)
+book = Product("Book", 401, 455874)
+laptop = Product("Laptop", 501, 45789)
+
 
 # Create movement class object
-m1 = Movement(l1, l2, p1, 78)
-m2 = Movement(l1, l3, p2, 45)
-m3 = Movement(l3, l4, p3, 23)
-m4 = Movement(l2, l1, p4, 41)
-m5 = Movement(l4, l3, p5, 85)
+m1 = Movement(rajkot, jamnagar, airconditioner, 78)
+m2 = Movement(rajkot, ahmadabad, cloths, 45)
+m3 = Movement(ahmadabad, baroda, chair, 23)
+m4 = Movement(jamnagar, rajkot, book, 41)
+m5 = Movement(baroda, ahmadabad, laptop, 85)
 
+print(".....Display product details with its stock at various locations using stock_at_locations.....")
+airconditioner.show_product()
+cloths.show_product()
+chair.show_product()
+book.show_product()
+laptop.show_product()
 
-p1.add(l1.name, m1.quantity)
-p2.add(l2.name, m2.quantity)
-p3.add(l3.name, m3.quantity)
-p4.add(l4.name, m4.quantity)
-p5.add(l1.name, m5.quantity)
+print()
+print(".....Display movements of each product using the movement_by_product method.....")
+mov_list = [m1, m2, m3, m4, m5]
+Movement.movements_by_product(airconditioner)
+Movement.movements_by_product(cloths)
+Movement.movements_by_product(chair)
+Movement.movements_by_product(book)
+Movement.movements_by_product(laptop)
 
-p1.details()
-p2.details()
-p3.details()
-p4.details()
-p5.details()
-
-m1.show_movement()
-m2.show_movement()
-m3.show_movement()
-m4.show_movement()
-m5.show_movement()
-
-Movement.movements_by_product(p2.name)
+print()
+print("......Display product list by location ( group by location)......")
+l_list = [rajkot, jamnagar, ahmadabad, baroda]
+Location.sort(l_list)
